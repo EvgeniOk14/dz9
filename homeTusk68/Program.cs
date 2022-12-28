4085ac5529aed1bbcffd2f9ceb32b7f86d65da5b
Evgeni.ok14 <evgeni.ok@mail.ru>
2022-12-28 10:16:17 +0300

    added anew project: homeTusk68

---
+++ homeTusk68/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число m: ");
+Double m = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите первое число n: ");
+double n = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("первое число равно " + m);
+Console.WriteLine("второе число равно " + n);
+double AkkermanRec(double m, double n)
+{
+  if (m == 0) return (n + 1);
+  else if (n == 0) return AkkermanRec(m-1, 1);
+  else  return AkkermanRec(m -1, AkkermanRec(m, n-1)); 
+}
+Console.WriteLine(AkkermanRec(m, n)); 
