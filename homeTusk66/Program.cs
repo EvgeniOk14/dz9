7460b6d3329e1a9f5c53a8b4cfaf2e17b7e08d4b
Evgeni.ok14 <evgeni.ok@mail.ru>
2022-12-28 10:11:29 +0300

    added a new project: homeTusk66

---
+++ homeTusk66/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите первое число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первое число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (m > n)
+{
+  Console.Write("Вы ввели первое число меньше второго! Введите заного! \n");
+  m = Convert.ToInt32(Console.ReadLine());
+  n = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("первое число равно " + m);
+Console.WriteLine("второе число равно " + n);
+int SumRec(int m, int n)
+{
+  if (m>n) return 0;
+  else return n + SumRec(m, n-1);
+}
+Console.WriteLine(SumRec(m, n));
